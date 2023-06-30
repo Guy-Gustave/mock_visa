8c1a1061a5dcb292aa93a89670e0dc7e45ac4315
RNtaate <royhyde14@gmail.com>
2023-06-30 16:09:53 +0300

    Replace the search container in home page to hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeroSection from '../components/HeroSection';
 import SearchContainer from '../components/SearchContainer';
 
 import styles from '../styles/Home.module.css';
@@ -6,7 +7,7 @@ import styles from '../styles/Home.module.css';
 const Home = () => {
   return (
     <section className={styles.homeSection}>
-      <SearchContainer />
+      <HeroSection />
     </section>
   );
 };
