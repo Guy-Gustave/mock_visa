c98608746278d080119338ffbd68173ecb00c84f
RNtaate <royhyde14@gmail.com>
2023-06-30 18:58:20 +0300

    Create a data file for the as Seen logos

---
+++ src/components/AsSeenComponents/asSeen.js
@@ -0,0 +1,55 @@
+import * as logoImages from '../../assets/asSeenImages';
+let logoData = [
+  {
+    source: logoImages.lonelyPlanet,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+  {
+    source: logoImages.lifeHacker,
+    pageLink:
+      'https://lifehacker.com/quickly-figure-out-what-the-visa-requirements-are-for-e-1831123185',
+  },
+  {
+    source: logoImages.esquire,
+    pageLink:
+      'https://www.esquiremag.ph/life/travel/visa-requirements-philippine-passport-a00200-20190103',
+  },
+  {
+    source: logoImages.travelNoire,
+    pageLink:
+      'https://travelnoire.com/travel-hack-how-to-find-the-visa-requirements-for-any-country/',
+  },
+  {
+    source: logoImages.gizmodo,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+  {
+    source: logoImages.productHunt,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+  {
+    source: logoImages.hackerNews,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+  {
+    source: logoImages.reddit,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+  {
+    source: logoImages.indieHackers,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+  {
+    source: logoImages.starterStory,
+    pageLink:
+      'https://media.lonelyplanet.com/shop/pdfs/the-digital-nomad-handbook-1-preview.pdf',
+  },
+];
+
+export default logoData;
