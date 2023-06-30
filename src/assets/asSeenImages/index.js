6098612829c41759b655ea86edc9e3f1b44bfa15
RNtaate <royhyde14@gmail.com>
2023-06-30 18:57:18 +0300

    Create an index file in assets folder to import the as seen logos to the project

---
+++ src/assets/asSeenImages/index.js
@@ -0,0 +1,23 @@
+import lonelyPlanet from './lonelyplanet.webp';
+import lifeHacker from './lifehacker.webp';
+import esquire from './esquire.webp';
+import travelNoire from './travelnoire.webp';
+import gizmodo from './gizmodo.webp';
+import productHunt from './producthunt.webp';
+import hackerNews from './hackernews.webp';
+import reddit from './reddit.webp';
+import indieHackers from './indiehackers.webp';
+import starterStory from './starterstory.webp';
+
+export {
+  lonelyPlanet,
+  lifeHacker,
+  esquire,
+  travelNoire,
+  gizmodo,
+  productHunt,
+  hackerNews,
+  reddit,
+  indieHackers,
+  starterStory,
+};
