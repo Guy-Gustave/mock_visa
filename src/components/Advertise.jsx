cf2b1ec164903d1fbcfc20369db7d50249ef060b
Guy <guymoustapha@gmail.com>
2023-07-02 23:58:57 +0200

    Add styles to Advertise component
    Add check Icon

--- src/components/Advertise.jsx
+++ src/components/Advertise.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
 import styles from "../styles/Advertise.module.css";
+import check from '../assets/check.svg'
 
 
 const Advertise = () => {
   return (
     <div className={styles.box}>
-        <h1>Join 292K travelers</h1>
-        <ul>
-            <li>
-                <p>icon</p>
+        <h1 className={styles.title}>Join 292K travelers</h1>
+        <ul className={styles.ulist}>
+            <li className={styles.elt}>
+                
+                <img className={styles.icon} src={check}alt="" />
                 <p>Unlock all filters</p>
             </li>
-            <li>
-                <p>icon</p>
+            <li className={styles.elt}>
+                <img className={styles.icon} src={check}alt="" />
                 <p>Member-only data</p>
             </li>
-            <li>
-                <p>icon</p>
+            <li className={styles.elt}>
+                <img className={styles.icon} src={check}alt="" />
                 <p>Ad-free experience</p>
             </li>
-            <li>
-                <p>icon</p>
+            <li className={styles.elt}>
+                <img className={styles.icon} src={check}alt="" />
                 <p>All new features</p>
             </li>
-            <li>
-                <p>icon</p>
+            <li className={styles.elt}>
+                <img className={styles.icon} src={check}alt="" />
                 <p> Keep track of travels</p>
             </li>
-            <li>
-                <p>icon</p>
+            <li className={styles.elt}>
+                <img className={styles.icon} src={check}alt="" />
                 <p> Community chat</p>
             </li>
 
         </ul>
-        <button>Unlock Travels Super Power</button>
+        <button className={styles.button}>Unlock Travels Super Power</button>
     </div>
   )
 }
