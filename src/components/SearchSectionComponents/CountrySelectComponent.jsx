0e1ca92e1b5e47f28e350ff141e5211f7693e7c1
RNtaate <royhyde14@gmail.com>
2023-07-02 18:28:50 +0300

    Add functionality for  selecting and updating the input with selected country

--- src/components/SearchSectionComponents/CountrySelectComponent.jsx
+++ src/components/SearchSectionComponents/CountrySelectComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import countries from '../../services/emojiFlagCountries.json';
 import styles from '../../styles/CountrySelectComponent.module.css';
@@ -6,6 +6,15 @@ import styles from '../../styles/CountrySelectComponent.module.css';
 const CountrySelectComponent = () => {
   const countriesList = [...countries.flagCountries];
   const [inputFocused, setInputFocused] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState(countriesList[0].name);
+  const inputRef = useRef();
+  const countriesRef = useRef();
+
+  const handleSelectListItem = (e) => {
+    inputRef.current.value = e.target.children[1].outerText;
+    setSelectedCountry(e.target.children[1].outerText);
+    countriesRef.current.blur();
+  };
 
   return (
     <div className={styles.countrySelectContainerDiv}>
@@ -13,15 +22,14 @@ const CountrySelectComponent = () => {
         className={`${styles.selectedCountriesDiv} ${
           inputFocused ? styles.selectedCountriesDivFocus : ''
         }`}
+        ref={countriesRef}
         tabIndex={-1}
         onFocus={(e) => {
-          if (!inputFocused) setInputFocused(true);
+          setInputFocused(true);
           e.target.placeholder = '';
-          console.log('I am the same guy called every time', e.target);
         }}
         onBlur={(e) => {
           setInputFocused(false);
-          console.log('I was hit for a split second');
           e.target.placeholder = 'Choose your passport country';
         }}
       >
@@ -30,6 +38,10 @@ const CountrySelectComponent = () => {
           className={styles.selectedCountryInput}
           placeholder="Choose your passport country"
           autoComplete="false"
+          name="Country"
+          defaultValue={selectedCountry}
+          onChange={() => {}}
+          ref={inputRef}
         />
         <div
           className={`${styles.countriesListDiv} ${
@@ -39,7 +51,14 @@ const CountrySelectComponent = () => {
           <ul className={styles.countriesList}>
             {countriesList.map((country) => {
               return (
-                <li key={country.name} className={styles.countryListItem}>
+                <li
+                  key={country.name}
+                  className={`${styles.countryListItem} ${
+                    selectedCountry == country.name ? styles.itemSelected : ''
+                  }`}
+                  onClick={handleSelectListItem}
+                  name={country.name}
+                >
                   <span>{country.flag}</span>
                   <p>{country.name}</p>
                 </li>
