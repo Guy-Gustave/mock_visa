bde67629051a76d2e75f1fe25175f683759a1d73
RNtaate <royhyde14@gmail.com>
2023-07-02 17:31:57 +0300

    Add functionality for facilitate clicking of input and showing countries list

--- src/components/SearchSectionComponents/CountrySelectComponent.jsx
+++ src/components/SearchSectionComponents/CountrySelectComponent.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
+import countries from '../../services/emojiFlagCountries.json';
 import styles from '../../styles/CountrySelectComponent.module.css';
 
 const CountrySelectComponent = () => {
+  const countriesList = [...countries.flagCountries];
   const [inputFocused, setInputFocused] = useState(false);
 
   return (
@@ -12,22 +14,39 @@ const CountrySelectComponent = () => {
           inputFocused ? styles.selectedCountriesDivFocus : ''
         }`}
         tabIndex={-1}
+        onFocus={(e) => {
+          if (!inputFocused) setInputFocused(true);
+          e.target.placeholder = '';
+          console.log('I am the same guy called every time', e.target);
+        }}
+        onBlur={(e) => {
+          setInputFocused(false);
+          console.log('I was hit for a split second');
+          e.target.placeholder = 'Choose your passport country';
+        }}
       >
         <input
           type="text"
           className={styles.selectedCountryInput}
           placeholder="Choose your passport country"
           autoComplete="false"
-          onFocus={(e) => {
-            setInputFocused(true);
-            e.target.placeholder = '';
-          }}
-          onBlur={(e) => {
-            setInputFocused(false);
-            e.target.placeholder = 'Choose your passport country';
-          }}
         />
-        <div className={styles.countriesListDiv}></div>
+        <div
+          className={`${styles.countriesListDiv} ${
+            inputFocused ? styles.countriesListDivShow : ''
+          }`}
+        >
+          <ul className={styles.countriesList}>
+            {countriesList.map((country) => {
+              return (
+                <li key={country.name} className={styles.countryListItem}>
+                  <span>{country.flag}</span>
+                  <p>{country.name}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
