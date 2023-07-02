b8cdfd8ab485b77d773d80bb13d344eed68072f1
RNtaate <royhyde14@gmail.com>
2023-07-02 15:03:27 +0300

    Implement selecting of a visa and displaying it in the paragraph
    Implement styling in accordance with visa selection

--- src/components/SearchSectionComponents/VisaSelectComponent.jsx
+++ src/components/SearchSectionComponents/VisaSelectComponent.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import coutries from '../../services/emojiFlagCountries.json';
 import visaTypes from '../../services/VisaTypes.json';
 import styles from '../../styles/VisaSelectComponent.module.css';
 
 const VisaSelectComponent = () => {
+  const visaTypesArray = [...visaTypes];
+  const [selectedVisa, setSelectedVisa] = useState(visaTypesArray[0]);
+
+  let selectDivRef = useRef();
+  const handleSettingVisa = (e) => {
+    console.log(e.target.outerText);
+    setSelectedVisa(e.target.outerText);
+    selectDivRef.current.blur();
+  };
+
   return (
     <div className={styles.visaSelectContainerDiv}>
-      <div className={styles.selectTypesDiv} tabIndex={-1}>
-        <p className={styles.selectedTypePar}>Uganda</p>
+      <div className={styles.selectTypesDiv} tabIndex={-1} ref={selectDivRef}>
+        <p className={styles.selectedTypePar}>{selectedVisa}</p>
         <div className={styles.visaTypesListDiv}>
           <ul className={styles.visaTypesList}>
             {visaTypes.map((type) => {
               return (
-                <li key={type} className={styles.visaListItem}>
-                  {type}
+                <li
+                  key={type}
+                  className={`${styles.visaListItem} ${
+                    selectedVisa == type.toString() ? styles.itemSelected : ''
+                  }`}
+                  onClick={(e) => handleSettingVisa(e)}
+                >
+                  {type.toString()}
                 </li>
               );
             })}
