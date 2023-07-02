168a7df25e03fff50f70b7a64e9eb4e5ecefb09c
RNtaate <royhyde14@gmail.com>
2023-07-02 20:58:01 +0300

    Remove unwanted code and format using prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import HeroSection from "../components/HeroSection";
-import SearchContainer from "../components/SearchContainer";
+import React from 'react';
+import HeroSection from '../components/HeroSection';
 
-import styles from "../styles/Home.module.css";
-import TopVisas from "../components/TopVisas";
-import Paragraphs from "../components/Paragraphs";
-import Advertise from "../components/Advertise";
+import styles from '../styles/Home.module.css';
+import TopVisas from '../components/TopVisas';
+import Paragraphs from '../components/Paragraphs';
+import Advertise from '../components/Advertise';
 
 const Home = () => {
   return (
@@ -13,11 +12,10 @@ const Home = () => {
       <section className={styles.homeSection}>
         <HeroSection />
         <TopVisas />
-        <Paragraphs/>
-        <Advertise/>
+        <Paragraphs />
+        <Advertise />
       </section>
-      <section>
-      </section>
+      <section></section>
     </>
   );
 };
